32b6fc1ebaa8c30a11f2da0d1e22380fde875787
Denis Voituron <denis@voituron.net>
2021-02-03 18:52:14 +0100

    Code WinForm migré en .NET5

--- Source/Common/DbContext.cs
+++ Source/Common/DbContext.cs
@@ -10,7 +10,7 @@ namespace MastermindCommon
         /// Dossier d'enregistrement des fichiers.
         /// !!! Il doit exister et le terminer pas un "\".
         /// </summary>
-        private readonly string GAME_PATH = @"C:\Temp\";
+        private readonly string GAME_PATH = GetGamePath();
 
         /// <summary>
         /// Enregistrement des résultats d'une partie, 
@@ -22,5 +22,15 @@ namespace MastermindCommon
             string json = JsonConvert.SerializeObject(game, Formatting.Indented);
             File.WriteAllText(GAME_PATH + $"{DateTime.Now:yyMMdd-HHmmss}-Mastermind.txt", json);
         }
+
+        private static string GetGamePath()
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Mastermind");
+            var value = Convert.ToString(key?.GetValue("GamePath") ?? @"C:\Temp\");
+            key?.Close();
+
+            return value;
+        }
+
     }
 }
